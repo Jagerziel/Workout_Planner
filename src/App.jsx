bdab5075b009a29a64d8c7a984b3bd4381b4038a
Jagerziel <fromnytosg@gmail.com>
2024-01-16 16:11:53 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Starting Page
+      </div>
+    </>
+  )
+}
+
+export default App
