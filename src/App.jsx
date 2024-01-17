9f705d9bd4ee67fe9daf66b80278e9bd42ccdeb6
Jagerziel <fromnytosg@gmail.com>
2024-01-17 12:51:54 -0500

    set up routes in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import './App.scss'
 import Navbar from './components/navbar/Navbar.jsx'
 
 // Lazy Load Components
+const Home = lazy(() => import('./screens/Home.jsx'))
+const TwelveWkPlan = lazy(() => import('./screens/TwelveWkPlan.jsx'))
+const Browse = lazy(() => import('./screens/Browse.jsx'))
+const Custom = lazy(() => import('./screens/Custom.jsx'))
+
 // const Navbar = lazy(() => import('./components/navbar/Navbar.jsx'))
 
 function App() {
@@ -15,7 +20,12 @@ function App() {
       <Navbar />
       <div className='Navbar-background'></div>
       <div className='App'>
-        Starting Page
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/twelvewkplan" element={<TwelveWkPlan />} />
+          <Route path="/browse" element={<Browse />} />
+          <Route path="/custom" element={<Custom />} />
+        </Routes>
       </div>
     </>
   )
---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+export const homeDirectory = [
+    {
+        title: '12 Week Workout Plan',
+        navigation: 'twelvewkplan'
+    },
+    {
+        title: 'browse',
+        navigation: 'twelvewkplan'
+    },
+    {
+        title: 'custom',
+        navigation: 'twelvewkplan'
+    },
+]
