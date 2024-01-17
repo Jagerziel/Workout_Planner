69deb2b6810f0bd700092dfa711abc9fadf6f3b9
Jagerziel <fromnytosg@gmail.com>
2024-01-17 13:51:46 -0500

    Shell code added for navigation items in home and 12 week plan

---
+++ src/components/directories/HomeItem.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './HomeItem.scss'
+
+export default function HomeItem() {
+  return (
+    <div className='HomeItem-container'>HomeItem</div>
+  )
+}
---
+++ src/components/directories/TwelveWkPlanItem.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './TwelveWkPlanItem.scss'
+
+export default function TwelveWkPlanItem() {
+  return (
+    <div className='TwelveWkPlanItem-container'>TwelveWkPlanItem</div>
+  )
+}
