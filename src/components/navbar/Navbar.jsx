a8ab70e75312b4d12b2a1c5cf10dae87fe0a4048
Jagerziel <fromnytosg@gmail.com>
2024-01-17 11:48:10 -0500

    Added backdrop for navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Navbar.scss'
+import Home from './images/home.png'
+
 
 export default function Navbar() {
 
     return (
         <>
             <div className='NavBar-Container'>
-                <div className='NavBar-home-button'></div>
+                <div 
+                    className='NavBar-home-button'
+                    onClick={() => console.log('Home Button Clicked')}
+                >
+
+                </div>
                 <div className='NavBar-Title'>Workout Planner</div>
 
             </div>
