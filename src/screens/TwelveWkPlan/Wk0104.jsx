69019747825820315f609d8576cd20166bfd10a9
Jagerziel <fromnytosg@gmail.com>
2024-01-22 16:45:13 -0500

    Updated show and hide for workouts in Wk0104

--- src/screens/TwelveWkPlan/Wk0104.jsx
+++ src/screens/TwelveWkPlan/Wk0104.jsx
@@ -22,37 +22,37 @@ export default function Wk0104() {
             title: "Day 2",
             workouts: [17,19,21,23,25,26,29,31,32,33],
             rest: false,
-            show: true,
+            show: false,
         },
         {
             title: "Day 3",
             workouts: [],
             rest: true,
-            show: true,
+            show: false,
         },
         {
             title: "Day 4",
             workouts: [34,35,36,37,39,41,43,44,45,46],
             rest: false,
-            show: true,
+            show: false,
         },
         {
             title: "Day 5",
             workouts: [],
             rest: true,
-            show: true,
+            show: false,
         },
         {
             title: "Day 6",
             workouts: [0,47,49,51,53,55,57,59],
             rest: false,
-            show: true,
+            show: false,
         },
         {
             title: "Day 7",
             workouts: [60,62,64,66,68,70,72,31,32,33],
             rest: false,
-            show: true,
+            show: false,
         },
     ])
 
@@ -66,13 +66,35 @@ export default function Wk0104() {
                         className='Wk0116-workout-container'
                         key={index}
                     >
-                        <div className='Wk0116-workout-day'>
-                            {data.title}
+                        <div 
+                            className='Wk0116-workout-day'
+                            onClick={() => {
+                                let shallowData = workoutIdx
+                                shallowData[index].show = !shallowData[index].show
+                                setWorkoutIdx([...shallowData])
+                            }
+                            }
+                        
+                        >
+                            {`${data.title} (${data.show ? ' - ' : ' + '})`}
                         </div>
                         {
                             data.rest ? 
-                            <RestItem /> :
-                            <WorkoutShellItem />
+                            <div style={{display: data.show ? "block" : "none"}}>
+                                <RestItem /> 
+                            </div> :
+                            <div 
+                                className='Wk0116-workout-item-container'
+                                style={{display: data.show ? "block" : "none"}}
+                            >
+                                {
+                                    data.workouts.map((workout, index) => 
+                                        <div key={index}> 
+                                            <WorkoutShellItem />
+                                        </div>
+                                    )
+                                }
+                            </div>
                         }
                     </div>
                 ))
