25d2299bb1d0cf4997a9ad0ed3f20d583b2a0d0d
Jagerziel <fromnytosg@gmail.com>
2024-01-22 15:49:52 -0500

    Moved workoutidx to useState in Wk0104

--- src/screens/TwelveWkPlan/Wk0104.jsx
+++ src/screens/TwelveWkPlan/Wk0104.jsx
@@ -1,10 +1,9 @@
 // Import React
-import React from 'react'
+import React, { useState } from 'react'
 // Import Components
 import RestItem from '../../components/workouts/RestItem.jsx'
 import WorkoutShellItem from '../../components/workouts/WorkoutShellItem.jsx'
 // Import Data
-import { weekDayDirectory } from '../../data/data.js'
 
 // Import CSS
 import './Wk0116.scss'
@@ -12,56 +11,69 @@ import './Wk0116.scss'
 // Import State Management
 
 export default function Wk0104() {
-    const workoutIdx = [
+    const [ workoutIdx, setWorkoutIdx ] = useState([
         {
             title: "Day 1",
             workouts: [0,1,3,5,6,9,10,13,14,15,16],
             rest: false,
+            show: true,
         },
         {
             title: "Day 2",
             workouts: [17,19,21,23,25,26,29,31,32,33],
             rest: false,
+            show: true,
         },
         {
             title: "Day 3",
             workouts: [],
             rest: true,
+            show: true,
         },
         {
             title: "Day 4",
             workouts: [34,35,36,37,39,41,43,44,45,46],
             rest: false,
+            show: true,
         },
         {
             title: "Day 5",
             workouts: [],
             rest: true,
+            show: true,
         },
         {
             title: "Day 6",
             workouts: [0,47,49,51,53,55,57,59],
             rest: false,
+            show: true,
         },
         {
             title: "Day 7",
             workouts: [60,62,64,66,68,70,72,31,32,33],
             rest: false,
+            show: true,
         },
-    ]
+    ])
+
 
     return (
         <div className='Wk0116-container'>
             <div className='Wk0116-title'>Weeks 1 - 4</div>
             {
-                weekDayDirectory.map(( data , index ) => (
+                workoutIdx.map(( data , index ) => (
                     <div 
                         className='Wk0116-workout-container'
                         key={index}
                     >
                         <div className='Wk0116-workout-day'>
-                            {data.day}
+                            {data.title}
                         </div>
+                        {
+                            data.rest ? 
+                            <RestItem /> :
+                            <WorkoutShellItem />
+                        }
                     </div>
                 ))
             }
