8551e04bceb4598fc88cf40b4e0fc632ced5f7c2
Jagerziel <fromnytosg@gmail.com>
2024-01-23 17:55:52 -0500

    Added conditional background formatting for exercise completion

--- src/components/workouts/WorkoutShellItem.jsx
+++ src/components/workouts/WorkoutShellItem.jsx
@@ -1,12 +1,30 @@
 // Import React
-import React from 'react'
+import React, { useState } from 'react'
 // Import CSS
 import './WorkoutShellItem.scss'
 
 export default function WorkoutShellItem( { exerciseDataItem } ) {
-  return (
-    <div className='WorkoutShellItem-container'>
-        {`${exerciseDataItem.title}`}
-    </div>
-  )
+    const [ completed, setCompleted ] = useState(false)
+
+
+    return (
+        <div 
+            className='WorkoutShellItem-container'
+            style={{
+                border: completed ? '2px solid green' : '2px solid white',
+                backgroundColor: completed ? '#2e492f' : '',
+            }}
+        >
+            {`${exerciseDataItem.title}`}
+            <div
+                className='WorkoutShellItem-completed-btn'
+                style={{
+                    backgroundColor: completed ? '#242424' : '#242424',
+                }}
+                onClick={() => setCompleted(prev => !prev)}
+            >
+            {completed ? 'Y' : 'N'}
+            </div>
+        </div>
+    )
 }
