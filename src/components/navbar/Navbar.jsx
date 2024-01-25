482cb439767d28ba9e58631e998a40c30575526a
Jagerziel <fromnytosg@gmail.com>
2024-01-24 21:56:41 -0500

    Fixed navbar issue

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,22 +6,19 @@ import { useNavigate } from 'react-router'
 export default function Navbar() {
     const navigate = useNavigate()
     return (
-        <>
-            <div className='NavBar-Container'>
-                <div 
-                    className='NavBar-home-button-container'
-                    onClick={() => navigate('/')}
-                >
-                    <img 
-                        src={HomeImg}
-                        alt="Project Img" 
-                        className='HomeImg'
-                        loading="lazy" 
-                    /> 
-                </div>
-                <div className='NavBar-Title'>Workout Planner</div>
-
+        <div className='NavBar-Container'>
+            <div 
+                className='NavBar-home-button-container'
+                onClick={() => navigate('/')}
+            >
+                <img 
+                    src={HomeImg}
+                    alt="Project Img" 
+                    className='HomeImg'
+                    loading="lazy" 
+                /> 
             </div>
-        </>
+            <div className='NavBar-Title'>Workout Planner</div>
+        </div>
     )
 }
