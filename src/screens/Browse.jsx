e0e4b06663e8c1b45a1f3a765d2294943ca54c5f
Jagerziel <fromnytosg@gmail.com>
2024-01-26 17:17:59 -0500

    Setup menu bar with onclick for future filtering

--- src/screens/Browse.jsx
+++ src/screens/Browse.jsx
@@ -1,17 +1,75 @@
 // Import React
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 // Import CSS
 import './Browse.scss'
 
 export default function Browse() {
+  const [ menu, setMenu ] = useState([
+    {
+      title: 'All',
+      tag: 'all',
+      selected: true,
+    },
+    {
+      title: 'Warmup',
+      tag: 'warmup',
+      selected: false,
+    },
+    {
+      title: 'Upper Body',
+      tag: 'upper',
+      selected: false,
+    },
+    {
+      title: 'Lower Body',
+      tag: 'lower',
+      selected: false,
+    },
+    {
+      title: 'Core',
+      tag: 'core',
+      selected: false,
+    },
+    {
+      title: 'Agility',
+      tag: 'agility',
+      selected: false,
+    },
+    {
+      title: 'Stretch',
+      tag: 'stretch',
+      selected: false,
+    },
+    {
+      title: 'Cardio',
+      tag: 'cardio',
+      selected: false,
+    }
+  ])
+
+  function handleMenuSelection ( tag ) {
+    console.log(tag)
+  }
+
   return (
     <div className='Browse-container'>
       <div className='Browse-menu-container'>
-        PLACEHOLDER FOR MENU
+        <div className='Browse-menu-subcontainer'>
+          {
+            menu.map((menuData, index) => 
+              <div 
+                className='Browse-menu-item-container'
+                onClick={() => handleMenuSelection(menuData.tag)}
+              >
+                <div className='Browse-menu-item-title'>{menuData.title}</div>
+              </div>
+            )
+          }
+        </div>
       </div>
-      <div className='Browse-workout-container'>
+      {/* <div className='Browse-workout-container'>
         PLACEHOLDER FOR WORKOUTS
-      </div>
+      </div> */}
     </div>
   )
 }
