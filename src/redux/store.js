49f4156824881a2d30f7841ebc2eb0e17eaf2883
Jagerziel <fromnytosg@gmail.com>
2024-02-05 12:21:26 -0500

    created reducer for cusom exercise data:

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit"
 import exerciseDataReducer from "./reducers/exerciseDataReducer";
 import homeDirectoryReducer from "./reducers/homeDirectoryReducer";
 import sixteenWkDirectoryReducer from "./reducers/sixteenWkDirectoryReducer";
+import customExerciseDataReducer from "./reducers/customExerciseDataReducer";
 
 export const store = configureStore({
     reducer: {
         exerciseData: exerciseDataReducer,
         homeDirectory: homeDirectoryReducer,
         sixteenWkDirectory: sixteenWkDirectoryReducer,
+        customExerciseData: customExerciseDataReducer,
     },
   });
--- src/screens/Custom.jsx
+++ src/screens/Custom.jsx
@@ -4,21 +4,35 @@ import React, { useRef, useState } from 'react'
 import './Custom.scss'
 // Import Framer Motion
 import PageTransitionFade from '../hooks/framer-motion/PageTransitionFade.jsx'
-
+// Import Redux
+import { useSelector, useDispatch } from "react-redux";
 
 export default function Custom() {
   const [ exercises, setExercises ] = useState([])
+  const [ selectedExercises, setSelectedExercises ] = useState([])
+  const createMenu = [
+    "create new workout",
+    "edit workout",
+  ]
 
+  const test = ["apple", "pear"]
+  const [apple, pear] = test
+  console.log(apple)
 
+  // Redux: Workout Data
+  const exerciseData = useSelector((state) => state.exerciseData.data)
 
   return (
     <PageTransitionFade>
-      <div>
-        <div>
+      <div className='Custom-container'>
+        <div className='Custom-title'>
           Customized Workout
         </div>
-        <div>
-
+        <div className='Custom-menu'>
+          {`(${selectedExercises.length === 0 ? createMenu[0] : createMenu[1]})`}
+        </div>
+        <div className='Custom-workout-container'>
+          CONTENT PLACEHOLDER
         </div>
       </div>
     </PageTransitionFade>
