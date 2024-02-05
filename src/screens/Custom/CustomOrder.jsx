45f3921e57442be2611326f0578f11126ecfe615
Jagerziel <fromnytosg@gmail.com>
2024-02-05 17:34:41 -0500

    Updated CustomOrder shell code and css with linked navigation

--- src/screens/Custom/CustomOrder.jsx
+++ src/screens/Custom/CustomOrder.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+// Import React
+import React, { useState } from 'react'
+import { useNavigate, NavLink } from 'react-router-dom'
+// Import CSS
 import './CustomOrder.scss'
+// Import Redux
+import { useSelector, useDispatch } from "react-redux";
 
 export default function CustomOrder() {
+    // Redux: Workout Data
+    const [selectedExercises, exerciseData] = useSelector((state) => state.customExerciseData.data)
+
+    // Navigation 
+    const navigate = useNavigate();
+
+
+    function handleConfirm () {
+        navigate('/custom')
+        console.log('navigation to browse button clicked')
+    }
+
     return (
         <div className='CustomOrder-container'>
-            CustomOrder
+            <div className='CustomOrder-title'>
+                Order Workouts
+            </div>
+            <div className='CustomOrder-exercise-container'>
+
+            </div>
+            <div className='CustomOrder-bottom-container'>
+                <div 
+                    className='CustomOrder-bottom-button'
+                    onClick={handleConfirm}
+                >
+                    Confirm
+                </div>
+            </div>
         </div>
     )
 }
