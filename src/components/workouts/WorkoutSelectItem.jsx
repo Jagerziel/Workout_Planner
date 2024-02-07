09bc11fdb0b59171c1bd13dafa76a79e84f40150
Jagerziel <fromnytosg@gmail.com>
2024-02-07 12:01:59 -0500

    Updated selection of items and ordering

--- src/components/workouts/WorkoutSelectItem.jsx
+++ src/components/workouts/WorkoutSelectItem.jsx
@@ -2,21 +2,53 @@
 import React, { useState } from 'react'
 // Import CSS
 import './WorkoutSelectItem.scss'
+// Import Redux
+import { useSelector, useDispatch } from "react-redux";
+import { updateCustomExerciseData } from '../../redux/reducers/customExerciseDataReducer.js';
 
 export default function WorkoutSelectItem( { exerciseDataItem } ) {
+    // Redux: Workout Data
+    const [selectedExercises, exerciseData] = useSelector((state) => state.customExerciseData.data)
+    // React Redux
+    const dispatch = useDispatch()
+    
     const [ workoutAdded, setWorkoutAdded ] = useState(false)
+
+    let selected = exerciseData[exerciseDataItem.id].selected
     
     function handleAdd () {
         setWorkoutAdded(prev => !prev)
+        let newExerciseDataItem = {...exerciseDataItem, selected: !exerciseData[exerciseDataItem.id].selected}
+        let exerciseDataShallowCopy = [...exerciseData]
+        let selectedExercisesShallowCopy = [...selectedExercises]
 
+        if (!workoutAdded) {
+            exerciseDataShallowCopy[exerciseDataItem.id] = newExerciseDataItem
+            selectedExercisesShallowCopy.push(exerciseDataItem.id)
+            dispatch(updateCustomExerciseData([selectedExercisesShallowCopy, exerciseDataShallowCopy]))
+
+            console.log([selectedExercisesShallowCopy, exerciseDataShallowCopy])
+        }
+        else {
+            exerciseDataShallowCopy[exerciseDataItem.id] = newExerciseDataItem
+            let adjustedArray = []
+            for (let i = 0; i < selectedExercisesShallowCopy.length; i++) {
+                if (selectedExercisesShallowCopy[i] !== exerciseDataItem.id) {
+                    adjustedArray.push(selectedExercisesShallowCopy[i])
+                }
+            }
+            dispatch(updateCustomExerciseData([adjustedArray, exerciseDataShallowCopy]))
+
+            console.log([adjustedArray, exerciseDataShallowCopy])
+        }
     }
-
+    
     return (
         <div 
             className='WorkoutSelectItem-container'
             style={{
-                border: workoutAdded ? '2px solid rgb(31,120,50)' : '2px solid white',
-                backgroundColor: workoutAdded ? '#2e492f' : '#242424',
+                border: selected ? '2px solid rgb(31,120,50)' : '2px solid white',
+                backgroundColor: selected ? '#2e492f' : '#242424',
             }}
         
         >
@@ -37,13 +69,9 @@ export default function WorkoutSelectItem( { exerciseDataItem } ) {
                 <div 
                     className='WorkoutSelectItem-button-container'
                     onClick={() => handleAdd()}
-                    style={{
-                        // border: `2px solid ${workoutAdded ? '#FF0000' : 'white'}`,
-                        // backgroundColor: `${workoutAdded ? 'rgba(105, 0, 0, 1)' : '#242424'}`
-                    }}
                 >
                     <div className='WorkoutSelectItem-button'>
-                        {`${workoutAdded ? '-' : '+'}`}
+                        {`${selected ? '-' : '+'}`}
                     </div>
                 </div>
             </div>
