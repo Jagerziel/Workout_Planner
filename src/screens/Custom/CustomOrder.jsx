caa25a0907c9de2967cc016c51bff5605af5610e
Jagerziel <fromnytosg@gmail.com>
2024-02-07 19:12:44 -0500

    Updated drag and drop - now impletementing redux to store sorted data

--- src/screens/Custom/CustomOrder.jsx
+++ src/screens/Custom/CustomOrder.jsx
@@ -1,5 +1,5 @@
 // Import React
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useNavigate, NavLink } from 'react-router-dom'
 // Import CSS
 import './CustomOrder.scss'
@@ -11,9 +11,46 @@ import { useSelector, useDispatch } from "react-redux";
 export default function CustomOrder() {
     // Redux: Workout Data
     const [selectedExercises, exerciseData] = useSelector((state) => state.customExerciseData.data)
+    const dispatch = useDispatch()
+
+    const [ sortedList, setSortedList ] = useState(selectedExercises)
+    const dragExercise = useRef(0)
+    const draggedOverExercise = useRef(0)
 
     // Navigation 
     const navigate = useNavigate();
+
+    function handleSort () {
+        let sortedListClone = [...sortedList]
+        // const temp = sortedListClone[dragExercise.current]
+        // sortedListClone[dragExercise.current] = sortedListClone[draggedOverExercise.current]
+        // sortedListClone[draggedOverExercise.current] = temp
+        
+        let dragExerciseIdx = dragExercise.current
+        let draggedOverExerciseIdx = draggedOverExercise.current
+        const listLen = sortedListClone.length
+        
+        if ( dragExerciseIdx > draggedOverExerciseIdx) {
+            let section01 = sortedListClone.slice(0, draggedOverExerciseIdx) // 0 to dragOverIndex
+            let section02 = sortedListClone[dragExerciseIdx] // insert dragged item
+            let section03 = sortedListClone.slice(draggedOverExerciseIdx, dragExerciseIdx) // dragOverIndex to dragged item
+            let section04 = sortedListClone.slice(dragExerciseIdx + 1) // skip dragged item (as it was inserted above) and return remainder of array
+            sortedListClone = [...section01, section02, ...section03, ...section04]
+        }
+        
+        if ( dragExerciseIdx < draggedOverExerciseIdx) {
+            let section01 = sortedListClone.slice(0, dragExerciseIdx) // 0 to dragged item
+            let section02 = sortedListClone.slice(dragExerciseIdx + 1, draggedOverExerciseIdx + 1) // skip dragged item and continue thru dragged over item
+            let section03 = sortedListClone[dragExerciseIdx] // insert dragged item
+            let section04 = sortedListClone.slice(draggedOverExerciseIdx + 1) // continue from item after draggedOver item and return remainder of array
+            sortedListClone = [...section01, ...section02, section03, ...section04]
+        }
+
+        
+        
+        setSortedList(sortedListClone)
+    }
+    
 
     function handleBack () {
         navigate('/custom/browse')
@@ -23,6 +60,8 @@ export default function CustomOrder() {
         navigate('/custom')
         console.log('navigation to browse button clicked')
     }
+
+    // console.log(dragExercise, draggedOverExercise)
 
     return (
         <div className='CustomOrder-container'>
@@ -35,12 +74,24 @@ export default function CustomOrder() {
             <div className='CustomOrder-exercise-container'>
                 <div className='CustomOrder-exercise-subcontainer'>
                     {
-                        selectedExercises.map((selectedIdx, index) => 
-                            <div key={index}>
-                                <WorkoutSelectItem exerciseDataItem={exerciseData[selectedIdx]} showSelect={false}/>
+                        sortedList.map((selectedIdx, index) => 
+                            <div 
+                                key={index}
+                                className='CustomOrder-exercise-item-container'
+                            >
+                                <div
+                                    className='CustomOrder-exercise-item-subcontainer'
+                                    draggable
+                                    onDragStart={() => dragExercise.current = index}
+                                    onDragEnter={() => draggedOverExercise.current = index}
+                                    onDragEnd={handleSort}
+                                    onDragOver={(e) => e.preventDefault()}
+                                >
+                                    <WorkoutSelectItem exerciseDataItem={exerciseData[selectedIdx]} showSelect={false}/>
+                                </div>
                                 <div
                                     style={{
-                                        marginTop: selectedIdx === selectedExercises[selectedExercises.length - 1] ? '0' : '10px'
+                                        marginTop: selectedIdx === sortedList[sortedList.length - 1] ? '0' : '10px'
                                     }}
                                 ></div>
                             </div>
