e4587a24d7396ecbe1f8c76725b53dbc01c0d4f2
Jagerziel <fromnytosg@gmail.com>
2024-02-13 10:03:27 -0500

    Fixed media queries on Sixteen week plan

--- src/screens/SixteenWkPlan.jsx
+++ src/screens/SixteenWkPlan.jsx
@@ -1,5 +1,5 @@
 // Import React
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 // Import CSS
 import './SixteenWkPlan.scss'
 // Import Components
@@ -11,10 +11,45 @@ import PageTransitionFade from '../hooks/framer-motion/PageTransitionFade.jsx';
 
 export default function SixteenWkPlan() {
 
+  // Size of Window
+  const [size, setSize] = useState({
+    x: window.innerWidth,
+    y: window.innerHeight
+  })
+
+  // Function to update size when window is resized
+  function updateSize () {
+    setSize({
+      x: window.innerWidth,
+      y: window.innerHeight
+    })
+  }
+
+  useEffect(() => (window.onresize = updateSize), [])
+
+  // Customized media query for react to override CSS
+  function mediaQuery ( columns ) {
+    if (size.x > 1800) return columns < 10 ? columns : 10
+    else if (size.x > 1600) return columns < 9 ? columns : 9
+    else if (size.x > 1400) return columns < 8 ? columns : 8
+    else if (size.x > 1230) return columns < 7 ? columns : 7
+    else if (size.x > 1100) return columns < 6 ? columns : 6
+    else if (size.x > 1000) return columns < 5 ? columns : 5
+    else if (size.x > 800) return columns < 4 ? columns : 4
+    else if (size.x > 560) return columns < 3 ? columns : 3
+    else if (size.x > 360) return columns < 2 ? columns : 2
+    else if (size.x >= 0) return columns < 1 ? columns : 1
+  }
+
   return (
     <PageTransitionFade>
       <div className='SixteenWkPlan-container'>
-        <div className='SixteenWkPlanItems-container'>
+        <div 
+          className='SixteenWkPlanItems-container'
+          style={{
+            '--m': mediaQuery(sixteenWkDirectory.length)
+          }}
+        >
           {sixteenWkDirectory.map((data, index) => <NavItem data={data} key={index} />)}
         </div>
       </div>
